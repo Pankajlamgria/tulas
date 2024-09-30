9bc559ff7f6eb4ae5179ac2a991748fe58715f47
amandeepta <amandiptaitis@gmail.com>
2024-09-30 17:05:34 +0530

    Added a simple routes

---
+++ backend/routes/getDetails.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+
+router.get('/getDetails', async (req, res) => {
+    try {
+        const id = req.body.id; 
+        const user = await User.findById({email : id});
+
+        if (!id || !user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        return res.status(200).json({
+            message: 'User details retrieved successfully',
+            user, 
+        });
+    } catch (error) {
+        return res.status(500).json({ message: 'Internal server error', error: error.message });
+    }
+});
+
+module.exports = router;
