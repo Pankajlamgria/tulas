0b786759a13e583d4549f730813c69639cb09c98
amandeepta <amandiptaitis@gmail.com>
2024-09-30 17:16:42 +0530

    Added the model changes

--- backend/models/user.js
+++ backend/models/user.js
@@ -9,29 +9,20 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
-        unique: true,  // Ensure email is unique
-        trim: true,    // To remove extra spaces
-        lowercase: true // Store email in lowercase
+        unique: true,
+        trim: true,
+        lowercase: true
     },
     password: {
         type: String,
-        required: true,
+        required: true
     },
-    age: {
-        type: Number,
-        required: true,
-        min: 0 // Optional: Ensure age is non-negative
-    },
-    height: {
-        type: Number,
-        required: true,
-        min: 0 // Optional: Ensure height is non-negative
-    },
-    weight: {
-        type: Number,
-        required: true,
-        min: 0 // Optional: Ensure weight is non-negative
+    details: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Details',
     }
 });
 
-export default mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
+
+export default User;
--- backend/routes/getDetails.js
+++ backend/routes/getDetails.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
+const User = require('../models/details');
 
 router.get('/getDetails', async (req, res) => {
     try {
